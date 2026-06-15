61608cdca0407a65a4d14b455407cad9ba0be74e
Phantom-J-A-T <ramonadedotun550@gmail.com>
2026-06-15 16:00:15 +0100

    feat: update social media links for X and LinkedIn in SocialIcons component

--- src/components/SocialIcons.tsx
+++ src/components/SocialIcons.tsx
@@ -4,7 +4,7 @@ function SocialIcons() {
 
             {/* X (formerly Twitter) */}
             <a
-                href="#"
+                href="https://x.com/getplugr"
                 target="_blank"
                 rel="noopener noreferrer"
                 className="w-10 h-10 rounded-full bg-[#333230] flex items-center justify-center text-white hover:bg-[#444341] transition-colors duration-200"
@@ -16,7 +16,7 @@ function SocialIcons() {
 
             {/* LinkedIn */}
             <a
-                href="#"
+                href="https://www.linkedin.com/company/getplugr/"
                 target="_blank"
                 rel="noopener noreferrer"
                 className="w-10 h-10 rounded-full bg-[#333230] flex items-center justify-center text-white hover:bg-[#444341] transition-colors duration-200"
